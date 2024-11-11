d079512bb37a83a77916d07b19ab57a57b77d817
Vinicius Ferreira <raymundo.vinicius@zup.com.br>
2024-11-11 12:07:59 -0300

    Create JWT Util class

---
+++ src/main/java/br/com/zup/user_manager/infra/security/JwtUtil.java
@@ -0,0 +1,52 @@
+package br.com.zup.user_manager.infra.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_kEY = "palavra_oculta_xablau";
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(SECRET_kEY).parseClaimsJwt(token).getBody();
+    }
+
+    private String createToken(Map<String, Object> claims, String subject){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10horas
+                .signWith(SignatureAlgorithm.HS256, SECRET_kEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token, String username){
+        final String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+}
